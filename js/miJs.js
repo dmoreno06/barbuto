b1d051fb4fa2ae0860c1e393b02237b8af7e2b8d
DMoreno_06 <mordiana1506@gmail.com>
2019-09-22 13:58:10 -0500

    galery services 90%

--- js/miJs.js
+++ js/miJs.js
@@ -1,21 +1,64 @@
- // function abrir(elemento) {
- //      document.getElementById("modal").src = elemento.src;
- //  }
+//funcion de boton hacia arriba
+jQuery(document).ready(function () {
 
-var email 	= require("./path/to/emailjs/email");
-var server 	= email.server.connect({
-   user:    "username",
-   password:"password",
-   host:    "smtp.your-email.com",
-   ssl:     true
+    var btn = $('#toTop');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+    condición ? expr1 : expr2 
 });
 
-// send the message and get a callback with an error or details of the message that was sent
-server.send({
-   text:    "i hope this works",
-   from:    "mordiana1506@gmail.com",
-   to:      "mordiana1506@gmail.com, another mordiana1506@gmail.com",
-   cc:      "else <mordiana1506@gmail.com>",
-   subject: "testing emailjs"
-}, function(err, message) { console.log(err || message); });
+        let num=1;
+		function leftBo(servico){
+            let ser= servico;
+			num--;
+			if(num<5)
+                num=1;
+            if(corte.click()){
+                console.log(document.getElementById(ser).id);
+                let ruta= ser.src="../img/destacados/"+ser+"/"+ser+num+".jpeg";
+                console.log(ruta);
+            }
+        }
+        function rightBo(servico){
+            let ser= servico;
+			num++;
+			if(num>5)
+                num=1;
+            if(corte.click()){
+                console.log(document.getElementById(ser).id);
+                let ruta= ser.src="../img/destacados/"+ser+"/"+ser+num+".jpeg";
+                console.log(ruta);
+            }
+        }
 
+        function valor(sevicio){
+            servivio = sevicio.id;
+            switch(servivio ) {
+                case "corte":
+                    console.log(document.getElementById("corte").id);
+                    leftBo("corte");
+                    break;
+                case "barba":
+                    console.log(document.getElementById("barba").id);
+                    leftBo("barba");
+                    break;
+                case "pigmentacion":
+                    console.log(document.getElementById("pigmentacion").id);
+                    leftBo("pigmentacion");
+                    break;
+                case "limpieza":
+                    console.log(document.getElementById("limpieza").id);
+                    leftBo("limpieza");
+                    break;
+                case "depilacion":
+                    console.log(document.getElementById("depilacion").id);
+                    leftBo("depilacion");
+                    break;
+            }          
+        }
